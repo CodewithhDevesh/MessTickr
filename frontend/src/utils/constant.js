1b03635997635a9f299070c42fa6b74a84789aed
Devesh <143711655+CodewithhDevesh@users.noreply.github.com>
2025-08-26 22:39:04 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const MEAL_API_END_POINT = `${BASE_URL}/mealconfirmation`;
